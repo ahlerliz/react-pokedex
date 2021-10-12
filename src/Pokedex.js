8bc9a01611221d37a76c5f06ae25af4aadaf34ab
davisrao <davisrao1@gmail.com>
2021-10-11 17:29:51 -0700

    pokegame scoring and playting

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,11 @@
 
 import Pokecard from "./Pokecard";
 
-function Pokedex({pokemonList}){
+function Pokedex({winner, pokemonList}){
     return(
         <div>
          {pokemonList.map(p => <Pokecard id={p.id} name={p.name} type={p.type} base_experience={p.base_experience}/>)}
+         <h3>{winner? "This hand Wins!" : "This hand Loses!"}</h3>
        </div>
     )
 }
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,18 +1,25 @@
 import pokemonList from "./pokemon.js"
 import Pokedex from "./Pokedex.js"
-import { generateHands } from "./helpers.js"
+import { generateHands, scoreHand } from "./helpers.js"
 
 function Pokegame() {
-    let hands = generateHands(pokemonList) // gives back {hand1, hand2}
+    let hands = generateHands(pokemonList); // gives back {hand1, hand2}
+    let hand1Score = scoreHand(hands.handOne);
+    let hand2Score = scoreHand(hands.handTwo);
+    let hand1Winner = hand1Score > hand2Score;
+    let hand2Winner = hand2Score > hand1Score;
+
     return (
         <div>
             <div>
-                <h1> Hand 1</h1>
-                <Pokedex pokemonList={hands.handOne} />
+                <h2> Hand 1 </h2>
+                <Pokedex winner={hand1Winner} pokemonList={hands.handOne} />
+                {/* <h1>{hand1Score > hand2Score ? 'This hand wins' : ''}</h1> */}
             </div>
             <div>
-                <h1> Hand 2</h1>
-                <Pokedex pokemonList={hands.handTwo} />
+                <h2> Hand 2 </h2>
+                <Pokedex winner={hand2Winner} pokemonList={hands.handTwo} />
+                {/* <h1>{hand2Score > hand1Score ? 'This hand wins' : ''}</h1>    */}
             </div>
         </div>
     )
